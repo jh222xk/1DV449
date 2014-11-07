58ddef218ea0a2cc0900e6b74285ddfb610962e7
Jesper Håkansson <jh222xk@student.lnu.se>
2014-11-07 10:15:40 +0100

    Added 'caching'

--- lab_01/main.py
+++ lab_01/main.py
@@ -12,6 +12,8 @@ from ghost import Ghost
 
 class global_data:
     DEBUG = False
+    CACHE_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+    DATA_FILE_NAME = 'data.json'
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
@@ -30,47 +32,59 @@ if __name__ == '__main__':
 
     data = list()
 
-    print "*** Scrapping, please wait..."
+    try:
+        with open(global_data.DATA_FILE_NAME) as dataFile:
+            jsonData = json.load(dataFile)
+        lastFetched = jsonData[-1].values()[0]
+        lastFetched = datetime.datetime.strptime(lastFetched, global_data.CACHE_DATETIME_FORMAT)
+    except Exception, e:
+        lastFetched = None
 
-    # Fetch as long as queue is not empty
-    while len(queue) > 0:
-        scraper = Ghost()
+    if lastFetched is None or lastFetched < datetime.datetime.now() - datetime.timedelta(minutes = 5):
+        print "*** Scrapping, please wait..."
 
-        # Get item from head of list
-        scraper.scrape(queue.pop(0))
-        # Add our new urls to the queue
-        queue += scraper.href
+        # Fetch as long as queue is not empty
+        while len(queue) > 0:
+            scraper = Ghost()
 
-        # Debugging
-        if global_data.DEBUG:
-            print 'Coursecode: %s' % scraper.courseCode if scraper.courseCode else 'No course code information'
-            print 'Title: %s' % scraper.title if scraper.title else 'No title information'
-            print 'URL: %s' % scraper.courseUrl if scraper.courseUrl else 'No url information'
-            print 'Courseplan: %s' % scraper.coursePlan if scraper.coursePlan else 'No course plan information'
-            print 'Ledtext: %s' % scraper.introText if scraper.introText else 'No intro information'
+            # Get item from head of list
+            scraper.scrape(queue.pop(0))
+            # Add our new urls to the queue
+            queue += scraper.href
 
-        # Append our data to the list
-        data.append({
-            'course_code': scraper.courseCode if scraper.courseCode else 'No course code information',
-            'title': scraper.title if scraper.title else 'No title information',
-            'course_url': scraper.courseUrl if scraper.courseUrl else 'No url information',
-            'course_plan': scraper.coursePlan if scraper.coursePlan else 'No course plan information',
-            'intro_text': scraper.introText if scraper.introText else 'No intro information',
-            'timestamp': datetime.datetime.now() if datetime.datetime.now else 'Timestamp failed'
-        })
+            # Debugging
+            if global_data.DEBUG:
+                print 'Coursecode: %s' % scraper.courseCode if scraper.courseCode else 'No course code information'
+                print 'Title: %s' % scraper.title if scraper.title else 'No title information'
+                print 'URL: %s' % scraper.courseUrl if scraper.courseUrl else 'No url information'
+                print 'Courseplan: %s' % scraper.coursePlan if scraper.coursePlan else 'No course plan information'
+                print 'Ledtext: %s' % scraper.introText if scraper.introText else 'No intro information'
 
-        if scraper.nextPage:
-            # Then fetch all the pages
-            queue += ["http://coursepress.lnu.se%s" % (scraper.nextPage)]
+            # Append our data to the list
+            data.append({
+                'course_code': scraper.courseCode if scraper.courseCode else 'No course code information',
+                'title': scraper.title if scraper.title else 'No title information',
+                'course_url': scraper.courseUrl if scraper.courseUrl else 'No url information',
+                'course_plan': scraper.coursePlan if scraper.coursePlan else 'No course plan information',
+                'intro_text': scraper.introText if scraper.introText else 'No intro information',
+                'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+            })
 
-    print "*** Done scrapping!"
-    print "*** Saving data..."
+            if scraper.nextPage:
+                # Then fetch all the pages
+                queue += ["http://coursepress.lnu.se%s" % (scraper.nextPage)]
 
-    # Get the number of courses fetched
-    data.append({'count': len(data)})
+        print "*** Done scrapping!"
+        print "*** Saving data..."
 
-    # Save the data to file
-    with open('data.json', 'w') as outfile:
-        json.dump(data, outfile, default=scraper.date_handler, indent=True, encoding='utf-8')
-    time.sleep(1)
-    print "*** Done saving"
+        # Get the number of courses fetched
+        data.append({'count': len(data)})
+        data.append({'last_fetch': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT)})
+
+        # Save the data to file
+        with open(global_data.DATA_FILE_NAME, 'w') as outfile:
+            json.dump(data, outfile, default=scraper.date_handler, indent=True, encoding='utf-8')
+        time.sleep(1)
+        print "*** Done saving"
+    else:
+        print "We still have some cached data, check '%s'" % global_data.DATA_FILE_NAME
