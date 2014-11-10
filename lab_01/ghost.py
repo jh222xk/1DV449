f06c470d273664f8ed812e5d9ee1de991324dc51
Jesper Håkansson <jh222xk@student.lnu.se>
2014-11-10 12:52:31 +0100

    Added other urls to scrape, like program, project and so on

--- lab_01/ghost.py
+++ lab_01/ghost.py
@@ -11,17 +11,26 @@ class Ghost:
     title = ''
     href = ''
     next_page = ''
-    course_detail = ''
-    course_code = ''
-    course_url = ''
-    course_plan = ''
     intro_text = ''
     latest_post_url = ''
     latest_post_title = ''
     latest_post_author = ''
     latest_post_time = ''
 
+    course_detail = ''
+    course_code = ''
+    course_url = ''
+    course_plan = ''
+
     program_urls = ''
+
+    project_urls = ''
+
+    subject_urls = ''
+
+    blog_urls = ''
+
+    authors = ['Johan Leitet', 'Daniel Toll', 'Peter Adiels']
 
     headers = {
         'User-Agent': 'Ghost',
@@ -40,23 +49,37 @@ class Ghost:
 
         page = requests.get(site, headers=self.headers)
 
+        # Our soup
         self.soup = BeautifulSoup(page.content)
 
-        # Course stuff
-        self.next_page = self.get_next_page()
+        # Common stuff
+        self.url = self.get_url()
         self.title = self.get_title()
-        self.href = self.get_href()
-        self.course_url = self.get_url()
-        self.course_code = self.get_course_code()
-        self.course_plan = self.get_course_plan()
         self.intro_text = self.get_entry_content()
         self.latest_post_url = self.get_latest_post_url()
         self.latest_post_title = self.get_latest_post_title()
         self.latest_post_author = self.get_latest_post_author()
         self.latest_post_time = self.get_latest_post_time()
 
+        # Next pages
+        self.next_page = self.get_next_page()
+
+        # Course stuff
+        self.href = self.get_hrefs("/kurs/")
+        self.course_code = self.get_course_code()
+        self.course_plan = self.get_course_plan()
+
         # Program stuff
-        self.program_urls = self.get_program_urls()
+        self.program_urls = self.get_hrefs("/program/")
+
+        # Project stuff
+        self.project_urls = self.get_hrefs("/projekt/")
+
+        # Subject stuff
+        self.subject_urls = self.get_hrefs("/subject/")
+
+        # Blog stuff
+        self.blog_urls = self.get_blog_urls()
 
     def get_next_page(self):
         """
@@ -131,6 +154,9 @@ class Ghost:
         return url
 
     def get_latest_post_url(self):
+        """
+        Get the url of the latest post made.
+        """
         posts = []
         try:
             ul = self.soup.find(
@@ -143,6 +169,9 @@ class Ghost:
         return posts
 
     def get_latest_post_author(self):
+        """
+        Get the author of the latest post made.
+        """
         try:
             authors = self.soup.find(
                 'p', {'class': 'entry-byline'}).find('strong').get_text()
@@ -151,6 +180,9 @@ class Ghost:
         return authors
 
     def get_latest_post_time(self):
+        """
+        Get the date of the latest post made.
+        """
         try:
             text = self.soup.find('p', {'class': 'entry-byline'}).get_text()
             match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}', text)
@@ -160,39 +192,43 @@ class Ghost:
         return date
 
     def get_latest_post_title(self):
+        """
+        Get the title of the latest post made.
+        """
         try:
             title = self.soup.find('h1', {'class': 'entry-title'}).get_text()
         except Exception, e:
             return ""
         return title
 
-    def get_program_urls(self):
+    def get_blog_urls(self):
         """
-        Get all the programs hrefs which we will fetch later on.
+        Get all the hrefs to the so called "blogs"
+        which we will fetch later on.
         """
         ul = self.soup.find('ul', {'class': 'item-list'})
         hrefs = []
         try:
             for title in ul.findAll('div', {'class': 'item-title'}):
                 for anchor in title.findAll('a'):
-                    # Get only the one with "program" in the url
-                    if "/program/" in anchor.get('href'):
+                    if anchor.get_text() in self.authors:
                         hrefs.append(anchor.get('href'))
         except Exception, e:
             pass
         return hrefs
 
-    def get_href(self):
+    def get_hrefs(self, specifier):
         """
-        Get all the courses hrefs which we will fetch later on.
+        Get all the hrefs with the given specifier like "/kurs/" or "/program/"
+        which we will fetch later on.
         """
         ul = self.soup.find('ul', {'class': 'item-list'})
         hrefs = []
         try:
             for title in ul.findAll('div', {'class': 'item-title'}):
                 for anchor in title.findAll('a'):
-                    # Get only the one with "kurs" in the url
-                    if "kurs" in anchor.get('href'):
+                    # Get only the one with "specifier" in the url
+                    if specifier in anchor.get('href'):
                         hrefs.append(anchor.get('href'))
         except Exception, e:
             pass
--- lab_01/main.py
+++ lab_01/main.py
@@ -30,6 +30,9 @@ class global_data:
     CACHE_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
     COURSE_DATA_FILE_NAME = 'course_data.json'
     PROGRAM_DATA_FILE_NAME = 'program_data.json'
+    PROJECT_DATA_FILE_NAME = 'project_data.json'
+    SUBJECT_DATA_FILE_NAME = 'subject_data.json'
+    BLOG_DATA_FILE_NAME = 'blog_data.json'
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
@@ -45,8 +48,11 @@ if __name__ == '__main__':
     queue = list()
     queue.append(args.url)
 
-    data = list()
+    course_data = list()
     program_data = list()
+    project_data = list()
+    subject_data = list()
+    blog_data = list()
 
     try:
         with open(global_data.COURSE_DATA_FILE_NAME) as data_file:
@@ -64,64 +70,158 @@ if __name__ == '__main__':
         while len(queue) > 0:
             scraper = Ghost()
 
-            # Get item from head of list
-            scraper.scrape(queue.pop(0))
+            # Get item from head of list which will be the current url
+            # which we will scrape.
+            current_url = queue.pop(0)
+
+            # Scrape it!
+            scraper.scrape(current_url)
+
             # Add our new urls to the queue
             queue += scraper.href
 
-            # print len(queue)
+            if "/kurs/" in current_url:
+                # Debugging
+                if global_data.DEBUG:
+                    print 'Coursecode: %s' % scraper.course_code if scraper.course_code else 'No course code information'
+                    print 'Title: %s' % scraper.title if scraper.title else 'No title information'
+                    print 'URL: %s' % scraper.url if scraper.url else 'No url information'
+                    print 'Courseplan: %s' % scraper.course_plan if scraper.course_plan else 'No course plan information'
+                    print 'Ledtext: %s' % scraper.intro_text if scraper.intro_text else 'No intro information'
+                    print 'Senaste post titel: %s' % scraper.latest_post_title if scraper.latest_post_title else 'No latest title information'
+                    print 'Senaste post skribent: %s' % scraper.latest_post_author if scraper.latest_post_author else 'No latest author information'
+                    print 'Senaste post datum: %s' % scraper.latest_post_time if scraper.latest_post_time else 'No latest date information'
 
-            # Debugging
-            if global_data.DEBUG:
-                print 'Coursecode: %s' % scraper.course_code if scraper.course_code else 'No course code information'
-                print 'Title: %s' % scraper.title if scraper.title else 'No title information'
-                print 'URL: %s' % scraper.course_url if scraper.course_url else 'No url information'
-                print 'Courseplan: %s' % scraper.course_plan if scraper.course_plan else 'No course plan information'
-                print 'Ledtext: %s' % scraper.intro_text if scraper.intro_text else 'No intro information'
-                print 'Senaste post titel: %s' % scraper.latest_post_title if scraper.latest_post_title else 'No latest title information'
-                print 'Senaste post skribent: %s' % scraper.latest_post_author if scraper.latest_post_author else 'No latest author information'
-                print 'Senaste post datum: %s' % scraper.latest_post_time if scraper.latest_post_time else 'No latest date information'
+                # Append our data to the list
+                course_data.append({
+                    'course_code': scraper.course_code if scraper.course_code else 'No course code information',
+                    'title': scraper.title if scraper.title else 'No title information',
+                    'url': scraper.url if scraper.url else 'No url information',
+                    'course_plan': scraper.course_plan if scraper.course_plan else 'No course plan information',
+                    'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
+                    'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
+                    'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
+                    'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
+                    'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+                })
 
-            # Append our data to the list
-            data.append({
-                'course_code': scraper.course_code if scraper.course_code else 'No course code information',
-                'title': scraper.title if scraper.title else 'No title information',
-                'course_url': scraper.course_url if scraper.course_url else 'No url information',
-                'course_plan': scraper.course_plan if scraper.course_plan else 'No course plan information',
-                'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
-                'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
-                'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
-                'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
-                'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
-            })
+            if scraper.program_urls:
+                queue += scraper.program_urls
+
+            if "/program/" in current_url:
+                program_data.append({
+                    'url': scraper.url if scraper.url else 'No url information',
+                    'title': scraper.title if scraper.title else 'No title information',
+                    'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
+                    'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
+                    'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
+                    'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
+                    'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+                })
+
+            if scraper.project_urls:
+                queue += scraper.project_urls
+
+            if "/projekt/" in current_url:
+                project_data.append({
+                    'url': scraper.url if scraper.url else 'No url information',
+                    'title': scraper.title if scraper.title else 'No title information',
+                    'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
+                    'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
+                    'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
+                    'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
+                    'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+                })
+
+            if scraper.subject_urls:
+                queue += scraper.subject_urls
+
+            if "/subject/" in current_url:
+                subject_data.append({
+                    'url': scraper.url if scraper.url else 'No url information',
+                    'title': scraper.title if scraper.title else 'No title information',
+                    'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
+                    'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
+                    'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
+                    'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
+                    'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+                })
+
+            if scraper.blog_urls:
+                queue += scraper.blog_urls
+
+            if scraper.title.strip() in scraper.authors:
+                blog_data.append({
+                    'url': scraper.url if scraper.url else 'No url information',
+                    'title': scraper.title if scraper.title else 'No title information',
+                    'intro_text': scraper.intro_text if scraper.intro_text else 'No intro information',
+                    'latest_post_title': scraper.latest_post_title if scraper.latest_post_title else 'No latest title information',
+                    'latest_post_author': scraper.latest_post_author if scraper.latest_post_author else 'No latest author information',
+                    'latest_post_date': scraper.latest_post_time if scraper.latest_post_time else 'No latest date information',
+                    'timestamp': datetime.datetime.now().strftime(global_data.CACHE_DATETIME_FORMAT) if datetime.datetime.now else 'Timestamp failed'
+                })
+
 
             if scraper.next_page:
                 # Then fetch all the pages
                 queue += ["http://coursepress.lnu.se%s" % (scraper.next_page)]
 
-            for program in scraper.program_urls:
-                program_data.append({
-                    'url': program,
-                    'title': scraper.title
-                })
-
         print "*** Done scrapping!"
         print "*** Saving data..."
 
         # Get the number of courses fetched
-        data.append({'count': len(data)})
-        data.append({'last_fetch': datetime.datetime.now().strftime(
+        course_data.append({'count': len(course_data)})
+        course_data.append({'last_fetch': datetime.datetime.now().strftime(
+            global_data.CACHE_DATETIME_FORMAT)})
+
+        # Get the number of programs fetched
+        program_data.append({'count': len(program_data)})
+        program_data.append({'last_fetch': datetime.datetime.now().strftime(
+            global_data.CACHE_DATETIME_FORMAT)})
+
+        # Get the number of projects fetched
+        project_data.append({'count': len(project_data)})
+        project_data.append({'last_fetch': datetime.datetime.now().strftime(
+            global_data.CACHE_DATETIME_FORMAT)})
+
+        # Get the number of subjects fetched
+        subject_data.append({'count': len(subject_data)})
+        subject_data.append({'last_fetch': datetime.datetime.now().strftime(
+            global_data.CACHE_DATETIME_FORMAT)})
+
+        # Get the number of blogs fetched
+        blog_data.append({'count': len(blog_data)})
+        blog_data.append({'last_fetch': datetime.datetime.now().strftime(
             global_data.CACHE_DATETIME_FORMAT)})
 
         # Sort the list of dicts by title of the courses
-        sortedList = sorted(data, key=lambda k: k.get('title'))
+        sorted_course_list = sorted(course_data, key=lambda k: k.get('title'))
+        sorted_program_list = sorted(program_data, key=lambda k: k.get('title'))
+        sorted_project_list = sorted(project_data, key=lambda k: k.get('title'))
+        sorted_subject_list = sorted(subject_data, key=lambda k: k.get('title'))
+        sorted_blog_list = sorted(blog_data, key=lambda k: k.get('title'))
 
         # Save the data to file
         save_data = SaveToJsonFile()
-        save_data.save_to_file(global_data.COURSE_DATA_FILE_NAME, sortedList)
+        save_data.save_to_file(global_data.COURSE_DATA_FILE_NAME, sorted_course_list)
+
+        # Save program data
         if program_data:
             save_data.save_to_file(
-                global_data.PROGRAM_DATA_FILE_NAME, program_data)
+                global_data.PROGRAM_DATA_FILE_NAME, sorted_program_list)
+
+        if project_data:
+            save_data.save_to_file(
+                global_data.PROJECT_DATA_FILE_NAME, sorted_project_list)
+
+        if subject_data:
+            save_data.save_to_file(
+                global_data.SUBJECT_DATA_FILE_NAME, sorted_subject_list)
+
+        if blog_data:
+            save_data.save_to_file(
+                global_data.BLOG_DATA_FILE_NAME, sorted_blog_list)
+
         time.sleep(1)
 
         print "*** Done saving"
